513467e4b312da524b884f0e3440e1f5d905a32f
ajain8616 <ajain8616@gmail.com>
2024-04-01 12:12:51 +0530

    New Code with fully Functionality

--- find-duplicate.py
+++ find-duplicate.py
@@ -11,51 +11,79 @@ def LoadFileList(path):
     global filesPath
     files = os.listdir(path)
     for file in files:
-        if os.path.isdir(path + "/" + file):
-            LoadFileList(path + "/" + file)
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            LoadFileList(file_path)
         else:
-            if filesPath.get(file) == None:
+            if filesPath.get(file) is None:
                 filesPath[file] = []
             filesPath[file].append(path)
 
 def LoadPriorityList():
     global FolderPriorityMap
-    df=pd.read_excel('files-priority.xlsx')
-    foldersPriority=np.array(df)
-    foldersPriorityList=foldersPriority.flatten()  
+    df = pd.read_excel('files-priority.xlsx')
+    foldersPriority = np.array(df)
+    foldersPriorityList = foldersPriority.flatten()  
     my_list = foldersPriority.tolist()
    
-    val=1
+    val = 1
     for i in my_list:
-        FolderPriorityMap[i[0]] = val
-        val = val + 1 
-
+        folder_name = i[0]
+        FolderPriorityMap[folder_name] = val
+        val += 1 
 
 def DeleteFile(file, folderlist):
     global FolderPriorityMap 
-    print("Deleting Duplicates of: " , file , " From ", folderlist)
-    # I need to browse through the folderlist
-    folder = folderlist[0]
-    val = FolderPriorityMap[folder]
-    # If the value of a flder is higher , than I will remove file from the folder
-    for fld in folderlist:
-        if FolderPriorityMap[fld] > val:
-            print("Remove From ", fld)
-        elif FolderPriorityMap[fld] < val:
-            print("Remove From ", folder)
-            folder = fld
-            val = FolderPriorityMap[folder]
+    print("Deleting Duplicates of:", file, "From", folderlist)    
+    folder = os.path.dirname(folderlist[0])  # Extract folder name
+    val = FolderPriorityMap.get(folder, 0)
+    deleted = False
     
+    for fld in folderlist:
+        current_folder = os.path.dirname(fld)  # Extract folder name
+        current_folder = current_folder if current_folder != '' else '.'  # Handle current_folder being ''
+        if FolderPriorityMap.get(current_folder, 0) > val:
+            print("Deleting from", fld)
+            os.remove(os.path.join(fld, file))  # Remove file
+            deleted = True
+        elif FolderPriorityMap.get(current_folder, 0) < val:
+            print("Deleting from", folder)
+            os.remove(os.path.join(folder, file))  # Remove file
+            folder = current_folder
+            val = FolderPriorityMap.get(folder, 0)
+            deleted = True
+    
+    if deleted:
+        print("Deleted:", file)
 
-def DeleteDuplicateFiles( ):
+def DeleteDuplicateFiles():
     global FolderPriorityMap
     global filesPath
     for filename in filesPath.keys():
         DeleteFile(filename, filesPath[filename])
 
-## Execution Strts Here
+def ShowDuplicateFiles():
+    for filename, folderlist in filesPath.items():
+        if len(folderlist) > 1:
+            print("Duplicate file:", filename)
+            print("Found in folders:")
+            for folder in folderlist:
+                print("-", folder)
+            print()
+
+def checkDuplicateFolderFiles():
+    has_duplicates = False
+    for filename, folderlist in filesPath.items():
+        if len(folderlist) > 1:
+            has_duplicates = True
+            break
+    
+    if has_duplicates:
+        DeleteDuplicateFiles()
+    else:
+        ShowDuplicateFiles()
+
+## Execution Starts Here
 LoadFileList("./test")
 LoadPriorityList()
-DeleteDuplicateFiles()
-
-
+checkDuplicateFolderFiles()
